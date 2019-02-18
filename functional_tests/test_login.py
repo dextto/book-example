53a38c923a9cdf16f4d9d6f7c5456d286691326e
dextto <zd.gary@gmail.com>
2019-02-18 21:56:03 +0900

    테스트 깨진것 수정

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.keys import Keys
 
 from .base import FunctionalTest
 
-TEST_EMAIL = 'dexter@rootone.com'
+TEST_EMAIL = 'edith@example.com'
 SUBJECT = 'Your login link for Superlists'
 
 
