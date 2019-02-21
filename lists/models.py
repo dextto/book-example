8fd6c0ca075510224e51ec7075bced48548d4d91
dextto <zd.gary@gmail.com>
2019-02-21 23:48:09 +0900

    add a name field to List

--- lists/models.py
+++ lists/models.py
@@ -6,6 +6,10 @@ from django.urls import reverse
 class List(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.CASCADE)
 
+    @property
+    def name(self):
+        return self.item_set.first().text
+
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
 
